6fb40e944acb3db4e0a0306cf1a42aa2256c2c98
Javier <82763996+javiicc@users.noreply.github.com>
2024-06-29 12:51:27 +0100

    feat: add some nvim config options

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,4 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.winbar = "%=%m %f"
+
+local opt = vim.opt
+
+opt.ignorecase = true -- ignore case in search patterns
+opt.number = false -- print the line number in front of each line
+opt.relativenumber = false -- show relative line number in front of each line
+opt.textwidth = 80 -- maximum width of text that is being inserted
+opt.colorcolumn = "80" -- columns to highlight
+opt.linebreak = true -- wrap long lines at a blank
